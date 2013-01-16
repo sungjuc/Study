a26c2338d29d2abb4041eb29bca8358790fead5b
SungJu Cho <scho@scho-mn.local>
2013-01-15 22:54:02 -0800

    Adding CS106A lecture 1 files.

---
+++ CS106A Programming Methdology/src/OurKarelProgram.java
@@ -0,0 +1,24 @@
+/*************************************************
+ * File: OurKarelProgram.java
+ *
+ * A simple Karel program where Karel picks up a
+ * beeper and carries it up a hill.
+ */
+
+import stanford.karel.*;
+
+public class OurKarelProgram extends Karel {
+	public void run() {
+		move();
+		pickBeeper();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		putBeeper();
+		move();
+	}
+}
