6c4fda08663703a32048eea0c819645494892803
SungJu Cho <sungjuc73@gmail.com>
2013-06-07 00:18:22 -0700

    Adding CS106B files.

---
+++ CS106B Programming Abstraction/assignment/Assignment1-xcode/3 - Numeric Conversions/NumericConversions.cpp
@@ -0,0 +1,25 @@
+/*
+ * File: NumericConversions.cpp
+ * ---------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Numeric Conversions problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include <string>
+#include "console.h"
+using namespace std;
+
+/* Function prototypes */
+
+string intToString(int n);
+int stringToInt(string str);
+
+/* Main program */
+
+int main() {
+    // [TODO: fill in the code]
+    return 0;
+}
